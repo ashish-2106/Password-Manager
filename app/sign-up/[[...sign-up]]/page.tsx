d70947d4ac08c574105f27e0aa92b8c41b4cf3b2
ashish-2106 <ashishjhajharia21@gmail.com>
2025-05-31 22:35:11 +0530

    updates

--- app/sign-up/[[...sign-up]]/page.tsx
+++
@@ -1,7 +0,0 @@
-import { SignIn, SignUp } from '@clerk/nextjs'
-
-export default function Page() {
-  return <div className='flex flex-col items-center justify-center h-screen'>
-<SignUp />
-  </div>
-}
